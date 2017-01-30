3ba253afe9b4c8f521adb5342f583ea5abdb1a50
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-01-30 16:35:56 +0300

    Contain Basetest for all tests.

---
+++ test/base.py
@@ -0,0 +1,15 @@
+import os
+from flask_testing import TestCase
+from run import app
+from config import base_dir
+
+
+class BaseTest(TestCase):
+	
+	def create_app(self):
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'test.db')
+		app.config['TESTING'] = True
+		app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
+		
+		return app
+
