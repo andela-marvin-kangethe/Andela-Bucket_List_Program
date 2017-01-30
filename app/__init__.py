f3738d556590df78f0538b8eec94c4c57399048e
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-01-30 23:19:13 +0300

    [Chore] Resolved the import issues.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from .. import config
+from .config import *
 
 db = SQLAlchemy()
 
@@ -12,7 +12,7 @@ def create_app(config_name):
     """
     application = Flask(__name__)
 
-    application.config.from_object(Config[config_name])
+    application.config.from_object(config[config_name])
 
     db.init_app(application)
 
