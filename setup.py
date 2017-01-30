41d07cb247a557995fd457af85120bcd46ea38e7
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-01-30 16:49:12 +0300

    [Feature] Modified to show project information.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+   name='Bucketlist API',
+   description='API for creating bucketlists for registered users',
+   author='Marvin Kang\'ethe',
+   author_email='marvin.kangethe@andela.com',
+   version='1.0.0'
+)
