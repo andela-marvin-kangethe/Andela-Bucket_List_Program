0f65a46ee8da4500efe74b6ce833d791b5f74636
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-01-30 23:14:09 +0300

    Initial commit.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from config import *
+from config import Config
 
 db = SQLAlchemy()
 
@@ -12,7 +12,7 @@ def create_app(config_name):
     """
     application = Flask(__name__)
 
-    application.config.from_object(config[config_name])
+    application.config.from_object(Config[config_name])
 
     db.init_app(application)
 
