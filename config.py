0a49b1ae5ec8a5f5189033396abb262a255b2e2d
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-01-31 11:47:44 +0300

    [Chore] Removed trailing whitespaces.

--- config.py
+++ config.py
@@ -1,22 +1,3 @@
-# # Statement for enabling the development environment
-# DEBUG = True
-
-# # Define the application directory
-# import os
-# BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
-
-# # Define the database - we are working with
-# # SQLite for this example
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-# DATABASE_CONNECT_OPTIONS = {}
-
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# # Secret key for signing cookies
-# SECRET_KEY = "secret"
-
-
-
 import os
 
 # Define the application directory
@@ -54,6 +35,7 @@ class StagingConfig(Config):
      environment properties
     """
     DEVELOPMENT = True
+    
     DEBUG = True
 
 
